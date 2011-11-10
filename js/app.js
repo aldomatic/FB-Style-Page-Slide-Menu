f491399ab462d7a2469a762aed1ba395c6506943
aldomatic <lugomatic@gmail.com>
2011-11-10 12:09:53 -0600

    Cleanded up the project

---
+++ js/app.js
@@ -0,0 +1,69 @@
+$(document).bind("mobileinit", function(){
+	$.mobile.pushStateEnabled = true;
+});
+
+
+
+
+
+		$(function(){
+			var menuStatus;
+			
+			
+			// Show menu
+			$("a.showMenu").click(function(){
+				if(menuStatus != true){				
+				$(".ui-page-active").animate({
+					marginLeft: "165px",
+				  }, 300, function(){menuStatus = true});
+				  return false;
+				  } else {
+					$(".ui-page-active").animate({
+					marginLeft: "0px",
+				  }, 300, function(){menuStatus = false});
+					return false;
+				  }
+			});
+			
+			
+			$('#menu, .pages').live("swipeleft", function(){
+				if (menuStatus){	
+				$(".ui-page-active").animate({
+					marginLeft: "0px",
+				  }, 300, function(){menuStatus = false});
+				  }
+			});
+			
+			$('.pages').live("swiperight", function(){
+				if (!menuStatus){	
+				$(".ui-page-active").animate({
+					marginLeft: "165px",
+				  }, 300, function(){menuStatus = true});
+				  }
+			});
+			
+			$('div[data-role="page"]').live('pagebeforeshow',function(event, ui){
+				menuStatus = false;
+				$(".pages").css("margin-left","0");
+			});
+			
+			// Menu behaviour
+			$("#menu li a").click(function(){
+				var p = $(this).parent();
+				if($(p).hasClass('active')){
+					$("#menu li").removeClass('active');
+				} else {
+					$("#menu li").removeClass('active');
+					$(p).addClass('active');
+				}
+			});
+		
+			// Tabs 
+			$('div[data-role="navbar"] a').live('click', function () {
+				$(this).addClass('ui-btn-active');
+				$('div.content_div').hide();
+				$('div#' + $(this).attr('data-href')).show();
+			});
+
+
+});	
---
+++ js/jquery.mobile.tabs.js
@@ -0,0 +1,96 @@
+/*
+* jQuery Mobile Framework : "tabs" plugin
+*/
+(function($, undefined ) {
+$.widget( "mobile.tabs", $.mobile.widget, {
+	options: {
+		iconpos: 'top',
+		grid: null,
+		load: function(event, ui) { },
+		beforeTabHide: function(event, ui) { },
+		beforeTabShow: function(event, ui) { },
+		afterTabShow:  function(event, ui) { }
+	},
+	_create: function(){
+		var
+			$this = this,
+			$tabs = this.element,
+			$navbtns = $tabs.find("a"),
+			iconpos = $navbtns.filter('[data-icon]').length ? this.options.iconpos : undefined;
+		var $content = $tabs.closest('div[data-role="page"]').find('div[data-role="content"]');
+
+		$tabs
+			.addClass('ui-navbar')
+			.attr("role","navigation")
+			.find("ul")
+				.grid({grid: this.options.grid });
+
+		if( !iconpos ){ 
+			$tabs.addClass("ui-navbar-noicons");
+		}
+
+		$navbtns
+			.buttonMarkup({
+				corners: false,
+				shadow:  false,
+				iconpos: iconpos
+			})
+			.removeClass('ui-link');
+
+		// Set up the direct children of the page as the tab content, hide them
+		$content.children().addClass('ui-tabs-content');
+		
+		// Now show the one that's active
+		if( $navbtns.filter('.ui-btn-active').length == 0 )
+			$navbtns.first().addClass('ui-btn-active');
+		$content.children('#' + $navbtns.eq($this.currentTab()).attr('href')).addClass('ui-tabs-content-active');
+
+		$navbtns.bind('click', function(event) {
+			navButtonClick.call(this, event);
+			return false;
+		})
+		.bind('tap', function(event){
+			navButtonClick.call(this, event);
+			return false;
+		});
+		
+		function navButtonClick(event) {
+			$navbtns.removeClass( "ui-btn-active" );
+			$( this ).addClass( "ui-btn-active" );
+			$this.changeTab(event, {
+				currentTab: $navbtns.eq($this.currentTab()),
+				nextTab: $(this),
+				currentContent: $this.currentContent(),
+				nextContent: $content.children($(this).attr('href'))
+			});
+			event.preventDefault();
+		}
+
+		this._trigger('load', null, {
+			currentTab: $navbtns.eq($this.currentTab()),
+			currentContent: $this.currentContent()
+		});
+	},
+	currentTab: function() {
+		var $tabs = this.element,
+		$navbtns = $tabs.find("a");
+		return this.element.find('.ui-btn-active').parent().prevAll().length;
+	},
+	currentContent: function() {
+		return this.element.closest('div[data-role="page"]').find('div[data-role="content"]').children().filter('.ui-tabs-content-active');
+	},
+	changeTab: function(event, ui) {
+		if( this._trigger('beforeTabHide', event, ui) )
+			ui.currentContent.siblings().andSelf().removeClass('ui-tabs-content-active');
+		if( this._trigger('beforeTabShow', event, ui) )
+			ui.nextContent.addClass('ui-tabs-content-active');
+		this._trigger('afterTabShow', event, $.extend({}, ui, { previousContent: ui.currentContent, currentContent: ui.nextContent, nextContent: null }));
+	}
+});
+})( jQuery );
+
+$(function() {
+	$('[data-role=page]').live('pagecreate', function(e) {
+		$(this).find('[data-role="tabs"]').tabs();
+	});
+});
